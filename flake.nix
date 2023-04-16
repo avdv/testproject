1c28ec61c284aef59d6d448b28daa6f3fe54b091
Claudio Bley <claudio.bley@gmail.com>
2023-04-16 23:39:58 +0200

    add empty gcc eh lib

--- flake.nix
+++ flake.nix
@@ -4,15 +4,16 @@
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
 
   outputs = { self, nixpkgs }:
-    let newShell = pkgs:
-      let
-        inherit (pkgs) pkgsStatic;
-        stdenvStatic = pkgsStatic.llvmPackages_11.libcxxStdenv;
-        mkShell = pkgsStatic.mkShell.override { stdenv = stdenvStatic; };
-      in
-      mkShell {
-        packages = with pkgs; [ which ];
-      };
+    let
+      newShell = pkgs:
+        let
+          inherit (pkgs) pkgsStatic;
+          stdenvStatic = pkgsStatic.llvmPackages_11.libcxxStdenv;
+          mkShell = pkgsStatic.mkShell.override { stdenv = stdenvStatic; };
+        in
+        mkShell {
+          packages = with pkgs; [ which ];
+        };
     in
     {
       packages.x86_64-darwin.default =
@@ -39,11 +40,22 @@
 
       packages.x86_64-linux.default =
         let
+          llvmVersion = import ./llvmVersion.nix;
+          addEmptyGccEh = final: prev: {
+            empty-gcc-eh = prev.runCommand "empty-gcc-eh" { } ''
+              mkdir -p $out/lib
+              ${prev.binutils-unwrapped}/bin/ar r $out/lib/libgcc_eh.a
+            '';
+            pkgsStatic."llvmPackages_${toString llvmVersion}".libcxxabi =
+              prev.pkgsStatic."llvmPackages_${toString llvmVersion}".libcxxabi.overrideAttrs (old: {
+                buildInputs = pkgsStatic."llvmPackages_${toString llvmVersion}".libcxxabi.buildInputs + [ final.empty-gcc-eh ];
+              });
+          };
           pkgs = import nixpkgs {
             system = "x86_64-linux";
+            overlays = [ addEmptyGccEh ];
           };
           inherit (pkgs) pkgsStatic;
-          llvmVersion = import ./llvmVersion.nix;
           stdenvStatic = pkgsStatic."llvmPackages_${toString llvmVersion}".libcxxStdenv;
           empty-gcc-eh = pkgs.runCommand "empty-gcc-eh" { } ''
             if $CC -Wno-unused-command-line-argument -x c - -o /dev/null <<< 'int main() {}'; then
@@ -67,16 +79,18 @@
         };
 
       devShells.x86_64-linux.default =
-        let pkgs = import nixpkgs {
-          system = "x86_64-linux";
-        };
+        let
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+          };
         in
         newShell pkgs;
 
       devShells.x86_64-darwin.default =
-        let pkgs = import nixpkgs {
-          system = "x86_64-darwin";
-        };
+        let
+          pkgs = import nixpkgs {
+            system = "x86_64-darwin";
+          };
         in
         newShell pkgs;
     };
