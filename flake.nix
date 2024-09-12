7fa46b0d06ef7de3d4574071f37adcdbee78eb18
Claudio Bley <claudio.bley@gmail.com>
2024-09-12 14:36:46 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let system = "x86_64-linux"; pkgs = import nixpkgs { inherit system; }; in {
+
+      apps.x86_64-linux.dockerBuild =
+        { program = "${self.packages.${system}.dockerImage}"; type = "app"; };
+
+      packages.${system}.dockerImage =
+        let
+          inherit (pkgs) dockerTools;
+          inherit (pkgs) bash python;
+        in
+        dockerTools.streamNixShellImage {
+          name = "nix-build";
+          drv = pkgs.mkShell.override { stdenv = pkgs.stdenvNoCC; }
+            {
+              PATH = pkgs.lib.makeBinPath [ pkgs.bash pkgs.coreutils ];
+              nativeBuildInputs = [ bash python ];
+            };
+          tag = "latest";
+        };
+    };
+}
